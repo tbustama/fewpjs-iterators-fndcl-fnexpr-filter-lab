c7792269966a6a1985c615ea3af6a436155af03f
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-01-18 20:42:26 -0800

    commit tree 2acbe96374c98e8e7ce5dfa5da6bfb78f71f3406

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(arr, str) {
+  return arr.filter((driver) => {
+    return driver.toLowerCase() === str.toLowerCase();
+  });
+}
+function fuzzyMatch(arr, str) {
+  return arr.filter((driver) => {
+    return driver.slice(0, str.length) === str;
+  });
+}
+
+function matchName(arr, str) {
+  return arr.filter((driver) => {
+    return driver.name === str;
+  });
+}
